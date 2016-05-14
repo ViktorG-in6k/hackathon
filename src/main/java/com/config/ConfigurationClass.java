0f06c7b50df235386c8ae79e53f61e1c9cf40fe3
ViktorG-in6k <ViktorG@softwareplanet.uk.com>
2016-05-14 12:27:18 +0300

    start config

---
+++ src/main/java/com/config/ConfigurationClass.java
@@ -0,0 +1,17 @@
+package main.java.com.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@Import(value = {WebAppConfig.class, OAuth2SecurityConfig.class})
+@ComponentScan(basePackages = {"com", "com.dataLayer.DAO.Implementations", "com.serviceLayer.service", "com.dataLayer.DAO"})
+public class ConfigurationClass {
+    @Bean
+    public PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
+
