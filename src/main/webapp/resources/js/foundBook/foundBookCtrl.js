19ab87d13c360d619aea40de6fad2481716db7d0
Denys Kudryashov <denysK@softwareplanet.uk.com>
2016-05-15 11:52:16 +0300

    MVP!!!

--- src/main/webapp/resources/js/foundBook/foundBookCtrl.js
+++ src/main/webapp/resources/js/foundBook/foundBookCtrl.js
@@ -1,13 +1,18 @@
 var module = angular.module('foundBookCtrl', []);
 
-module.controller('FoundBookCtrl', ['$scope', '$location','FoundBook',
-    function ($scope, $location, FoundBook) {
+module.controller('FoundBookCtrl', ['$scope', '$location','FoundBook', 'Notification',
+    function ($scope, $location, FoundBook, Notification) {
 
         $scope.addBook = function() {
             FoundBook.save({id: $scope.bookId})
                 .$promise
-                .then(function () {
-                    $location.path('/books/' + $scope.bookId);
+                .then(function (data) {
+                    if(data) {
+                        $location.path('/books/' + $scope.bookId);
+                        Notification('Congratulations! You have found the book!');
+                    } else {
+                        Notification('This book is not free!!!');
+                    }
                 });
         }
     }]);
--- src/main/webapp/resources/js/profile/profileCtrl.js
+++ src/main/webapp/resources/js/profile/profileCtrl.js
@@ -1,10 +1,15 @@
 var module = angular.module('profileCtrl', []);
 
-module.controller('profileCtrl', ['$scope', 'UserTransfers',
-    function ($scope, UserTransfers) {
+module.controller('profileCtrl', ['$scope', '$location', 'UserTransfers', 'Notification',
+    function ($scope, $location, UserTransfers, Notification) {
         $scope.transfers = UserTransfers.query();
 
-        $scope.pushTransfer =  function (transfer) {
-            UserTransfers.save({id: transfer.id}, transfer);
+        $scope.pushTransfer = function (transfer) {
+            UserTransfers.save({id: transfer.id}, transfer)
+                .$promise
+                .then(function () {
+                    $location.path('/');
+                    Notification('Your book is free!');
+                });
         }
     }]);
